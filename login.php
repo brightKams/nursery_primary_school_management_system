95808427e621efc14c6003db3d62e2730159c853
brightkams <ohakambright@gmail.com>
2024-04-16 04:33:28 +0100

    update commit for signup password hashing to the database

--- login.php
+++ login.php
@@ -25,36 +25,32 @@
     if (empty(validateInput($_POST["username"])) || empty(validateInput($_POST["password"]))) {
       echo "Complete the empty Field(s)";
       header('Location: login.html');
-    } 
-    
-    else {
+      exit();
+    } else {
       $username = validateInput($_POST["username"]);
       $password = validateInput($_POST["password"]);
       // $sql = "SELECT username FROM users WHERE username = '$username' and password = '$password' ";
-      $sql="SELECT roleid, Accountstatus,password_hash FROM users WHERE username='$username' and password='$password'";
+      $sql = "SELECT roleid, Accountstatus,password_hash FROM users WHERE username='$username' and password='$password'";
 
       $result = mysqli_query($db, $sql);
-      $row=mysqli_fetch_array($result);
+      $row = mysqli_fetch_array($result);
 
       // if (mysqli_num_rows($result) == 1)
-      if(mysqli_num_rows($result) == 1 && $row["roleid"]=="1" && $row["Accountstatus"]=="1" && password_verify($_POST["password"], $row["password_hash"]) )
-      
-      {
+      if (mysqli_num_rows($result) == 1 && $row["roleid"] == "1" && $row["Accountstatus"] == "1" && password_verify($_POST["password"], $row["password_hash"])) {
 
         session_start();
         header("Location: dashboard.php");
-      } 
-      elseif (mysqli_num_rows($result) == 0) {
+        exit();
+      } elseif (mysqli_num_rows($result) == 0) {
         echo "Incorrect Username or Password 
         <br> <a href=\"login.html\"> Login <a>";
-        exit;
-      }
-      else {
+        exit();
+      } else {
         echo "<h1>Incorrect Password or email </h1>";
         header("Location: login.php");
+        exit();
       }
     }
-
   }
 
 
--- signup.php
+++ signup.php
@@ -3,6 +3,66 @@
   include("connection.php");
   include("validateInput.php");
 
+//  $username=$_POST['username'];
+//  $firstname=$_POST['firstname'];
+//  $lastname=$_POST['lastname'];
+//  $email=$_POST['email'];
+//  $password=$_POST['password'];
+//  $confirmPassword=$_POST['confirmPassword'];
+ 
 
-  // header("Location: signup.html")
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+      if (empty(validateInput($_POST["fName"])) || empty(validateInput($_POST["lName"])) || empty(validateInput($_POST["email"])) || empty(validateInput($_POST["username"])) || empty(validateInput($_POST["password"])) || empty(validateInput($_POST["confirmPassword"]))) {
+        echo "Complete the empty Field(s)";
+        header('Location: signup.html');
+        exit();
+      } elseif (validateInput($_POST['password']) !==  validateInput($_POST["confirmPassword"])) {
+        echo "Password does not match Confirm Password ";
+        exit();
+        // header("Location: signup.html");
+      }
+
+      else {
+        $first_name = validateInput($_POST["fName"]);
+        $last_name = validateInput($_POST["lName"]);
+        $email = validateInput($_POST["email"]);
+        $username = validateInput($_POST["username"]);
+        $password = validateInput($_POST["password"]);
+        $confirm_password = validateInput($_POST["confirmPassword"]);
+        $password_hash = password_hash($password, PASSWORD_DEFAULT);
+       
+        $sql =  "INSERT INTO users (username, password_hash, email, roleid) VALUES ('$username', '$password_hash', '$email', '1')" ;
+        $sql2 = "INSERT INTO persons (firstname, lastname,  email)  VALUES ('$first_name', '$last_name', '$email')" ;
+        $stmt = $db->stmt_init();
+  #####
+  // $stmt_user = $db->prepare($sql);
+  // $stmt_person = $db->prepare($sql2);
+
+  if (!$stmt->prepare($sql)) {
+    die("SQL error: " . $db->error);
+}
+
+
+  #####
+
+        $result = mysqli_query($db, $sql) ;
+        $result2 = mysqli_query($db, $sql2) ;
+  
+       if ($result && $result2) {
+        echo "Registered Successfully" ;
+        header("Location: login.html");
+       }
+       
+        else {
+          echo "<h1>Please register with correcct details! </h1>";
+          header("Location: signup.html");
+          exit();
+        }
+      }
+    }
+    ##############
+
+
+
 ?>
